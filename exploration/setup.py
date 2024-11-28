0eab45655dc22565ff24515d2993b51b21700621
Bigenlight <tpingouin@gmail.com>
2024-11-28 21:00:58 +0900

    최태오 커밋

---
+++ exploration/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'exploration'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        # Install package.xml
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        # Include launch files if any
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # Include package.xml
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'numpy'],
+    zip_safe=True,
+    maintainer='theo',
+    maintainer_email='tpingouin@gmail.com',
+    description='Frontier-based exploration package for TurtleBot4.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'exploration_node = exploration.exploration:main',
+        ],
+    },
+)
